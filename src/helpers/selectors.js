45a2af97022e402d6936dae1f05ce7fbc15b1d65
kai-commits <kaimeikle123@gmail.com>
2022-04-21 16:00:17 -0700

    add getAppointmentsForDay helper func and tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export const getAppointmentsForDay = (state, day) => {
+  let appointmentIds = [];
+  let results = [];
+  for (const item of state.days) {
+    if (item.name === day) {
+      appointmentIds = item.appointments;
+    }
+  }
+  for (const id of appointmentIds) {
+    const appointmentsKeys = Object.keys(state.appointments);
+    for (const key of appointmentsKeys) {
+      if (id === Number(key)) {
+        results.push(state.appointments[id]);
+      }
+    }
+  }
+  return results;
+}
